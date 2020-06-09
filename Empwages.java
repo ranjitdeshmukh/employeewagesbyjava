eb58d545ee49e5f802ff745e90c29ee297b7c94e
Shubham <ranjitdeshmukh342@gmail.com>
2020-06-09 21:24:31 +0530

    Added : Employee present or absent functionality

---
+++ Empwages.java
@@ -0,0 +1,17 @@
+	public class Empwages {
+
+		public static void main(String[]args){
+
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HR=20;
+		
+		double empCheck = Math.floor(Math.random()*10) % 2;
+		if (empCheck == IS_FULL_TIME ){ 
+			System.out.print("present");
+		}else{
+			System.out.print("absent");
+	       }
+
+	     }
+
+	}
