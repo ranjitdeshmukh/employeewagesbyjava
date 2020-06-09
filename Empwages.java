480c7c8868e7017a0e9dbd23dc91b83eee52f7cd
Shubham <ranjitdeshmukh342@gmail.com>
2020-06-09 22:13:02 +0530

    Added : part time and full time calculation

--- Empwages.java
+++ Empwages.java
@@ -3,17 +3,26 @@
 		public static void main(String[]args){
 
 		int IS_FULL_TIME=1;
-	
-		double empCheck = Math.floor(Math.random()*10) % 2;
-		if (empCheck == IS_FULL_TIME ){ 
-			int EMP_RATE_PER_HR=20;
-			int empHrs=8;
-			double salary = (EMP_RATE_PER_HR*empHrs);
-			System.out.print(salary);
-		}else{
-			int salary=0; 
-			System.out.print("absent");
-	       }
+		int IS_PART_TIME=2;
+		int EMP_RATE_PER_HR=20;
+
+		int empHrs=0;
+
+		double empCheck = Math.floor(Math.random()*10) % 3;
+		if (empCheck == IS_FULL_TIME ){ 			
+			 empHrs=8;
+		}else 
+
+		if (empCheck == IS_PART_TIME){
+			 empHrs=4; 
+	    }
+	    else{
+
+           empHrs=0;
+	    	
+	    }
+	        double salary = empHrs * EMP_RATE_PER_HR  ;
+	        System.out.print(salary);
 
 	     }
 
