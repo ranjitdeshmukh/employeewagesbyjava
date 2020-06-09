a84eb684ec9b7ed3e454ca2b8e50f57f2e934ae2
Shubham <ranjitdeshmukh342@gmail.com>
2020-06-09 21:34:23 +0530

    Added  : Calculating the daily wages of employee

--- Empwages.java
+++ Empwages.java
@@ -3,12 +3,15 @@
 		public static void main(String[]args){
 
 		int IS_FULL_TIME=1;
-		int EMP_RATE_PER_HR=20;
-		
+	
 		double empCheck = Math.floor(Math.random()*10) % 2;
 		if (empCheck == IS_FULL_TIME ){ 
-			System.out.print("present");
+			int EMP_RATE_PER_HR=20;
+			int empHrs=8;
+			double salary = (EMP_RATE_PER_HR*empHrs);
+			System.out.print(salary);
 		}else{
+			int salary=0; 
 			System.out.print("absent");
 	       }
 
