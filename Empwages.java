4b7eacd31052cb2e5e03b4c74fbf72fa7ec918b3
Shubham <ranjitdeshmukh342@gmail.com>
2020-06-09 22:42:02 +0530

    Added : Calculating the monthly wages

--- Empwages.java
+++ Empwages.java
@@ -6,8 +6,14 @@
 		int IS_PART_TIME=2;
 		int EMP_RATE_PER_HR=20;
 
-		int empHrs=0;
 
+		int empHrs=0;
+		int numberofworkingday=20;
+		double totalsalary=0;
+
+		for (int day=1; day<=numberofworkingday ; day++) {
+			
+		
 		double empCheck = Math.floor(Math.random()*10) % 3;
 		
 		switch((int) empCheck) {
@@ -22,8 +28,11 @@
 		   		empHrs=0;
         }
 	        double salary = empHrs * EMP_RATE_PER_HR  ;
-	        System.out.print(salary);
+	        totalsalary = salary + totalsalary;
+	        
+	  	}
 
-	     }
+
+	  }
 
 	}
