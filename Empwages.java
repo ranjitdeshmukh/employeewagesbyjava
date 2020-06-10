536c0c1790861173696405fce12e1ef180c6e1b9
Shubham <ranjitdeshmukh342@gmail.com>
2020-06-10 08:39:50 +0530

    Added : Calculating the wages till date reached or not

--- Empwages.java
+++ Empwages.java
@@ -2,37 +2,38 @@
 
 		public static void main(String[]args){
 
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int EMP_RATE_PER_HR=20;
+			int IS_FULL_TIME=1;
+			int IS_PART_TIME=2;
+			int EMP_RATE_PER_HR=10;
+			int NUM_Working_Day=20;
 
+			int empHrs=0;
+			int numberofworkingday=0;
+			double totalsalary=0;
 
-		int empHrs=0;
-		int numberofworkingday=20;
-		double totalsalary=0;
+			while(numberofworkingday<=NUM_Working_Day) {
 
-		for (int day=1; day<=numberofworkingday ; day++) {
-			
-		
-		double empCheck = Math.floor(Math.random()*10) % 3;
-		
-		switch((int) empCheck) {
-		
-			case 1 : 			
-			      empHrs=8;
-			      break;
-		    case 2 :
-		    	  empHrs=4;
-		    	  break;
-		   default :
-		   		empHrs=0;
-        }
-	        double salary = empHrs * EMP_RATE_PER_HR  ;
-	        totalsalary = salary + totalsalary;
-	        
-	  	}
+				double empCheck = Math.floor(Math.random()*10) % 3;
 
+				switch((int) empCheck) {					
+					case 1 : 			
+					empHrs=8;
+					break;
 
-	  }
+					case 2 :
+					empHrs=4;
+					break;
+
+					default :
+					empHrs=0;
+				}
+
+				double salary = empHrs * EMP_RATE_PER_HR  ;
+				totalsalary = salary + totalsalary;
+				numberofworkingday++;
+
+			}
+			System.out.println(totalsalary);
+		}
 
 	}
